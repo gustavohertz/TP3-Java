c54a0f2e6a6a9aee50e9847286c7d06c79d8c2f2
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-11-19 20:03:24 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,7 +1,14 @@
+package org.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.Scanner;
 
 public class JogoDaVelha {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JogoDaVelha.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Iniciando o jogo...");
+
         char[][] tabuleiro = {
                 {' ', ' ', ' '},
                 {' ', ' ', ' '},
@@ -12,26 +19,24 @@ public class JogoDaVelha {
         char jogadorAtual = 'X';
 
         while (jogoEmAndamento) {
-            // Exibir o tabuleiro
             exibirTabuleiro(tabuleiro);
 
-            // Obter a entrada do jogador
             fazerJogada(tabuleiro, jogadorAtual);
 
-            // Verificar se há um vencedor ou empate
             if (verificarVencedor(tabuleiro, jogadorAtual)) {
                 exibirTabuleiro(tabuleiro);
-                System.out.println("Jogador " + jogadorAtual + " venceu!");
+                LOGGER.info("Jogador {} venceu!", jogadorAtual);
                 jogoEmAndamento = false;
             } else if (verificarEmpate(tabuleiro)) {
                 exibirTabuleiro(tabuleiro);
-                System.out.println("O jogo empatou!");
+                LOGGER.info("O jogo empatou!");
                 jogoEmAndamento = false;
             }
 
-            // Alternar o jogador
             jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
         }
+
+        LOGGER.info("Encerrando o jogo...");
     }
 
     public static void exibirTabuleiro(char[][] tabuleiro) {
@@ -60,53 +65,42 @@ public class JogoDaVelha {
             coluna = scanner.nextInt();
 
             if (linha >= 0 && linha < 3 && coluna >= 0 && coluna < 3 && tabuleiro[linha][coluna] == ' ') {
-                // A jogada é válida, atualize o tabuleiro
                 tabuleiro[linha][coluna] = jogador;
                 break;
             } else {
-                System.out.println("Jogada inválida. Tente novamente.");
+                LOGGER.error("Jogada inválida. Tente novamente.");
             }
         }
     }
-
 
     public static boolean verificarVencedor(char[][] tabuleiro, char jogador) {
-            // Verifique as linhas
-            for (int linha = 0; linha < 3; linha++) {
-                if (tabuleiro[linha][0] == jogador && tabuleiro[linha][1] == jogador && tabuleiro[linha][2] == jogador) {
-                    return true; // O jogador venceu na linha
-                }
-            }
-
-            // Verifique as colunas
-            for (int coluna = 0; coluna < 3; coluna++) {
-                if (tabuleiro[0][coluna] == jogador && tabuleiro[1][coluna] == jogador && tabuleiro[2][coluna] == jogador) {
-                    return true; // O jogador venceu na coluna
-                }
-            }
-
-            // Verifique as diagonais
-            if (tabuleiro[0][0] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][2] == jogador) {
-                return true; // O jogador venceu na diagonal principal
-            }
-
-            if (tabuleiro[0][2] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][0] == jogador) {
+        for (int linha = 0; linha < 3; linha++) {
+            if (tabuleiro[linha][0] == jogador && tabuleiro[linha][1] == jogador && tabuleiro[linha][2] == jogador) {
                 return true;
             }
-
-            return false;
         }
 
-        public static boolean verificarEmpate(char[][] tabuleiro) {
-            for (int linha = 0; linha < 3; linha++) {
-                for (int coluna = 0; coluna < 3; coluna++) {
-                    // Se encontrar qualquer posição vazia, o jogo ainda não empatou
-                    if (tabuleiro[linha][coluna] == ' ') {
-                        return false;
-                    }
-                }
+        for (int coluna = 0; coluna < 3; coluna++) {
+            if (tabuleiro[0][coluna] == jogador && tabuleiro[1][coluna] == jogador && tabuleiro[2][coluna] == jogador) {
+                return true;
             }
-            // Se chegou até aqui, o jogo empatou
+        }
+
+        if (tabuleiro[0][0] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][2] == jogador) {
             return true;
         }
+
+        return tabuleiro[0][2] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][0] == jogador;
     }
+
+    public static boolean verificarEmpate(char[][] tabuleiro) {
+        for (int linha = 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 3; coluna++) {
+                if (tabuleiro[linha][coluna] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
